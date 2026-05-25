540773cfbca7a53ec693911ddc28f031486e49ff
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-05-25 15:00:36 +0530

    day_3 2_questions array_n_hashing

--- array_n_hashing/src/sort_colors.rs
+++ array_n_hashing/src/sort_colors.rs
@@ -1,8 +1,14 @@
 #![allow(dead_code)]
 
 pub fn sort_colors(nums: &mut Vec<i32>) {
+    // The index zero and one will point to the respective indices where the value
+    // of 0 and 1 elements should be. The value 2 will keep getting written and
+    // re-written with small values until there are none, which by then means the
+    // array is sorted.
+    // See the printed results for better understanding.
     let (mut zero, mut one) = (0, 0);
     for two in 0..nums.len() {
+        println!("{:?}", nums);
         let tmp = nums[two];
         nums[two] = 2;
         if tmp < 2 {
